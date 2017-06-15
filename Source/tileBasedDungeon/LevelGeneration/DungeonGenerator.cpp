a66685f1d3d65f2ac5dd53cd76e7335dba9d1d4c
Paul Buschmann <paul.buschmann@chello.at>
2017-06-15 21:18:54 +0200

    Update @DungeonGenerator
    Add functions
    *AddRooms not finished
    *SetBlockAt finished

--- Source/tileBasedDungeon/LevelGeneration/DungeonGenerator.cpp
+++ Source/tileBasedDungeon/LevelGeneration/DungeonGenerator.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "tileBasedDungeon.h"
 #include "DungeonGenerator.h"
-
 
 // Sets default values
 ADungeonGenerator::ADungeonGenerator()
@@ -18,8 +18,10 @@ ADungeonGenerator::ADungeonGenerator()
 
 	stage_ = new EBlockType[stage_size_];
 
+	num_room_tries_ = 10;
+
 	// create default subobjects for instanced static meshes
-	wall_ = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("testmesh"));
+	wall_ = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Wall"));
 	wall_->SetupAttachment(RootComponent);
 }
 
@@ -32,8 +34,9 @@ void ADungeonGenerator::BeginPlay()
 	{ // fill everyslot with wall
 		stage_[i] = EBlockType::EWall;
 	}
+	AddRooms();
 
-	SpawnInstancedStage();
+	SpawnInstancedStage(); // last step
 }
 
 // Called every frame
@@ -47,7 +50,7 @@ void ADungeonGenerator::SpawnInstancedStage()
 	for (int i = 0; i < GetStageSize(); i++)
 	{
 		EBlockType current_block = stage_[i];
-		FVector current_location(i / GetStageHeight() * TILE_SIZE_, i % GetStageHeight() * TILE_SIZE_, 10.f);
+		FVector current_location(i / GetStageWidth() * TILE_SIZE_, i % GetStageWidth() * TILE_SIZE_, TILE_SIZE_);
 		switch (current_block)
 		{
 		case EBlockType::EFloor:
@@ -65,6 +68,64 @@ void ADungeonGenerator::SpawnInstancedStage()
 			break;
 		}
 	}
+}
+
+void ADungeonGenerator::AddRooms()
+{
+	for (int i = 0; i < num_room_tries_; i++)
+	{
+		// pick a random roomsize
+		// avoid too narrow rooms
+		// set it odd sized to line up with maze
+		int size = FMath::RandRange(1, 3) * 2 + 1;
+		int rectangularity = FMath::RandRange(0, 1 + size / 2) * 2;
+		int width = size;
+		int height = size;
+		if (FMath::RandBool())
+		{
+			width += rectangularity;
+		}
+		else
+		{
+			height += rectangularity;
+		}
+		//TODO maybe switch x with y
+
+		// set room origin point 
+		int x = FMath::RandRange(0, (stage_width_ - width) / 2) * 2 + 1;
+		int y = FMath::RandRange(0, (stage_height_ - height) / 2) * 2 + 1;
+
+		Room new_room(x, y, width, height);
+
+		bool overlaps = false;
+
+		for (Room other : rooms_)
+		{ // search for overlapping rooms
+			if (new_room.DistanceToOther(other) <= 0.f) // should be the piecemeal distanceTo function //TODO TODO
+			{
+				overlaps = true;
+				break;
+			}
+		}
+		if (overlaps) continue; // overlapping room! -> try to place new room
+
+		rooms_.push_back(new_room);
+		//TODO increase region
+
+		for (int pos_x = new_room.x_; pos_x < (new_room.x_ + new_room.width_); pos_x++)
+		{
+			for (int pos_y = new_room.y_; pos_y < (new_room.y_ + new_room.height_); pos_y++)
+			{
+				SetBlockAt(FVector2D(pos_x, pos_y));
+			}
+		}
+	}
+}
+
+void ADungeonGenerator::SetBlockAt(FVector2D position, EBlockType type)
+{
+	int i = position.X + stage_width_ * position.Y;
+	stage_[i] = type;
 }
 
 void ADungeonGenerator::BeginDestroy()
--- Source/tileBasedDungeon/LevelGeneration/DungeonGenerator.h
+++ Source/tileBasedDungeon/LevelGeneration/DungeonGenerator.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "GameFramework/Actor.h"
+#include <vector>
+#include "Room.h"
 #include "DungeonGenerator.generated.h"
 
 UENUM()
@@ -25,9 +27,17 @@ private:
 	/** pointer to store the rectangular stage array in */
 	EBlockType* stage_; 
 
+	/** place to store the stage's rooms */
+	std::vector<Room> rooms_;
+
 	unsigned int stage_size_; // 0 to 65,535
-	int stage_width_; // has to be odd
-	int stage_height_; // has to be odd
+	int stage_width_; // has to be odd along y-axis
+	int stage_height_; // has to be odd along x-axis
+
+	UPROPERTY(EditAnywhere, Category = leveldata)
+	int num_room_tries_; // numer of times a room tries to get placed
+
+
 
 protected:
 	UPROPERTY(EditDefaultsOnly, Category = leveldata)
@@ -38,6 +48,15 @@ protected:
 private:
 	/** spawn InstancedStaticMesh actors for values inside the array */
 	void SpawnInstancedStage();
+
+	/** add randomly oriented rooms to the stage array */
+	void AddRooms();
+
+	/** sets tile to type or floor
+	* @param position the position to carve out
+	* @param type the blocktype to set (if NULL will be set to floor)
+	*/
+	void SetBlockAt(FVector2D position, EBlockType type = EBlockType::EFloor);
 
 public:	
 	// Sets default values for this actor's properties
