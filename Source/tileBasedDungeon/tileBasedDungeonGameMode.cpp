ef6c462fe6a820974460e3f1ea3c197123582d04
Paul Buschmann <paul.buschmann@chello.at>
2018-01-31 18:01:14 +0100

    Update @HUD @Gamemode
      *added blueprint for CPP Character HUD
      *added gamemode blueprint class based on c++ for easier configuration

--- Source/tileBasedDungeon/tileBasedDungeonGameMode.cpp
+++ Source/tileBasedDungeon/tileBasedDungeonGameMode.cpp
@@ -7,13 +7,5 @@
 
 AtileBasedDungeonGameMode::AtileBasedDungeonGameMode(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
-	ConstructorHelpers::FObjectFinder<UBlueprint> PlayerBlueprint(TEXT("Blueprint'/Game/Blueprints/BluePrintCharacter.BluePrintCharacter'"));
-	if (PlayerBlueprint.Object)
-	{
-		DefaultPawnClass = Cast<UClass>(PlayerBlueprint.Object->GeneratedClass);
-	}
-
-	PlayerControllerClass = ADungeonPlayerController::StaticClass();
-	HUDClass = ADungeonHUD::StaticClass();
 }
 
