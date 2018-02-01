538e6d1e5598f8dc4703db2f5fc2b4ca6125fa11
Paul Buschmann <paul.buschmann@chello.at>
2018-02-01 14:01:13 +0100

    Add @Projectile
      *added base class for projectiles

---
+++ Source/tileBasedDungeon/Projectiles/Projectile.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "tileBasedDungeon.h"
+#include "Projectile.h"
+
+
+
+// Sets default values
+AProjectile::AProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	speed_ = 800.f;
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FTimerHandle DummyTimerHandle;
+	GetWorldTimerManager().SetTimer(DummyTimerHandle, this, &AProjectile::EndLifecycle, 1.0f);
+
+}
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	{
+		FVector location = GetActorLocation();
+		location += (DeltaTime * speed_) * GetTransform().GetUnitAxis(EAxis::X);
+		SetActorLocation(location);
+	}
+}
+
+void AProjectile::EndLifecycle_Implementation()
+{
+	Destroy();
+}
---
+++ Source/tileBasedDungeon/Projectiles/Projectile.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class TILEBASEDDUNGEON_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectile();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** gets called when the lifecycle ends */
+	UFUNCTION(BlueprintNativeEvent, Category = "Projectile")
+	void EndLifecycle();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile")
+	float speed_;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+
+
+};
